ab1a8a5aed7eda83ed27698dac6a580b7aca475c
devteamaegis <devteam.aegis@gmail.com>
2026-06-01 14:56:54 -0400

    feat: add /about dedicated page

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { StarField, Nav, AboutSection, LogosSection, VX_GLOBAL_STYLE } from '../page'
+
+export default function AboutPage() {
+  return (
+    <>
+      <style>{VX_GLOBAL_STYLE}</style>
+      <StarField />
+      <Nav active="about" />
+      <div style={{ paddingTop: 64, position: 'relative', zIndex: 1 }}>
+        <AboutSection />
+        <LogosSection />
+      </div>
+    </>
+  )
+}
