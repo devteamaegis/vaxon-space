15fb0fa3d705cd4053ae2d9c839ad172461bf930
devteamaegis <devteam.aegis@gmail.com>
2026-06-01 14:57:06 -0400

    feat: add /team dedicated page

---
+++ app/team/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { StarField, Nav, TeamSection, TeamMember, CORE_TEAM, ADVISORS, VX_GLOBAL_STYLE } from '../page'
+
+export default function TeamPage() {
+  const [cmsCoreTeam, setCmsCoreTeam] = useState<TeamMember[] | null>(null)
+  const [cmsAdvisors, setCmsAdvisors] = useState<TeamMember[] | null>(null)
+
+  useEffect(() => {
+    fetch('/api/cms').then(r => r.json()).then(d => {
+      if (d.team?.length) {
+        setCmsCoreTeam(d.team.filter((m: TeamMember) => !m.isAdvisor))
+        setCmsAdvisors(d.team.filter((m: TeamMember) => m.isAdvisor))
+      }
+    }).catch(() => {})
+  }, [])
+
+  const core     = cmsCoreTeam ?? CORE_TEAM
+  const advisors = cmsAdvisors ?? ADVISORS
+
+  return (
+    <>
+      <style>{VX_GLOBAL_STYLE}</style>
+      <StarField />
+      <Nav active="team" />
+      <div style={{ paddingTop: 64, position: 'relative', zIndex: 1 }}>
+        <TeamSection core={core} advisors={advisors} />
+      </div>
+    </>
+  )
+}
